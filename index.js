30a4d3ef2a6b7e6791beed421d90e79a28aa396d
Mariusz <mariusz.kwiecien2@onet.pl>
2023-06-04 13:45:45 +0200

    Like

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+'use strict';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
